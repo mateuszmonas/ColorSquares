58b8e4188b11c5c62f16235a0e8de628b25d04a7
mateuszmonas <mateuszmonas@gmail.com>
2019-12-24 18:06:08 +0100

    improved ui deisgn

--- src/main/java/com/gmail/mateuszmonas/game/view/BoardField.java
+++ src/main/java/com/gmail/mateuszmonas/game/view/BoardField.java
@@ -1,17 +1,25 @@
 package com.gmail.mateuszmonas.game.view;
 
+import javafx.geometry.Insets;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
-public class BoardField extends Button {
+import java.awt.*;
+
+public class BoardField extends Pane {
 
     public BoardField(double prefWidth, double prefHeight) {
         setPrefWidth(prefHeight);
         setPrefHeight(prefHeight);
-        setText("0");
     }
 
     public void update(int i) {
-        setText(String.valueOf(i));
+        setBackground(new Background(new BackgroundFill(Color.rgb(i*125, 0, 0), CornerRadii.EMPTY, Insets.EMPTY)));
     }
 
 }
