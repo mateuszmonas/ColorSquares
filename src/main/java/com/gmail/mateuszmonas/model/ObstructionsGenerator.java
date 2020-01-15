cd113448f4135244d30b517c0b7b5c02c618c348
mateuszmonas <mateuszmonas@gmail.com>
2020-01-15 17:31:58 +0100

    implemented union set

--- src/main/java/com/gmail/mateuszmonas/model/ObstructionsGenerator.java
+++ src/main/java/com/gmail/mateuszmonas/model/ObstructionsGenerator.java
@@ -8,41 +8,48 @@ public class ObstructionsGenerator {
 
     private final static int UNVISITED = -1;
 
-    static void dfs(Field field, int subGraphNo, int[][] visited) {
-        Deque<Field> stack = new ArrayDeque<>();
-        visited[field.x][field.y] = subGraphNo;
-        stack.add(field);
-        while (!stack.isEmpty()) {
-            Field currentField = stack.pop();
-            for (Field field1 : currentField.getAdjacent()) {
-                if (visited[field1.x][field1.y]==UNVISITED && !field1.isBlocked()) {
-                    visited[field1.x][field1.y] = subGraphNo;
-                    stack.add(field1);
-                }
-            }
-        }
-    }
 
-    static boolean isDisconnected(Field[][] board) {
-        int[][] visited = new int[board.length][board[0].length];
-        int subGraphNo = UNVISITED;
-        for (int[] ints : visited) {
-            Arrays.fill(ints, UNVISITED);
-        }
-        for (Field[] fields : board) {
-            for (Field field : fields) {
-                if (visited[field.x][field.y] == UNVISITED && !field.isBlocked()) {
-                    if (subGraphNo > UNVISITED) return true;
-                    dfs(field, ++subGraphNo, visited);
-                }
-            }
-        }
-        return false;
-    }
 
     static void generateObstructions(int obstructionsCount, GameBoard gameBoard) {
     }
 
+    private class boundaryUnionSet{
+        private int[][] boundary;
 
+        public boundaryUnionSet(int width, int height) {
+            boundary = new int[width][height];
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    boundary[i][j] = i + j;
+                }
+            }
+            for (int i = 0; i < width; i++) {
+                boundary[i][0] = 0;
+                boundary[i][height - 1] = 0;
+            }
+            for (int i = 0; i < height; i++) {
+                boundary[0][i] = 0;
+                boundary[width-1][i] = 0;
+            }
+        }
+
+        public int findParent(int x, int y) {
+            int parentX = boundary[x][y] % boundary.length;
+            int parentY = boundary[x][y] / boundary.length;
+            if (boundary[x][y] == boundary[parentX][parentY]) {
+                return boundary[x][y];
+            }
+            int parent = findParent(parentX, parentY);
+            boundary[x][y] = parent;
+            return parent;
+        }
+
+        public void union(int x1, int y1, int x2, int y2) {
+            int parent1X = findParent(x1, y1) % boundary.length;
+            int parent1Y = findParent(x1, y1) / boundary.length;
+            boundary[parent1X][parent1Y] = findParent(x2, y2);
+        }
+
+    }
 
 }
--- src/test/java/com/gmail/mateuszmonas/model/ObstructionsGeneratorTest.java
+++ src/test/java/com/gmail/mateuszmonas/model/ObstructionsGeneratorTest.java
@@ -3,13 +3,47 @@ package com.gmail.mateuszmonas.model;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObstructionsGeneratorTest {
 
+    private final static int UNVISITED = -1;
 
+    static void dfs(Field field, int subGraphNo, int[][] visited) {
+        Deque<Field> stack = new ArrayDeque<>();
+        visited[field.x][field.y] = subGraphNo;
+        stack.add(field);
+        while (!stack.isEmpty()) {
+            Field currentField = stack.pop();
+            for (Field field1 : currentField.getAdjacent()) {
+                if (visited[field1.x][field1.y]==UNVISITED && !field1.isBlocked()) {
+                    visited[field1.x][field1.y] = subGraphNo;
+                    stack.add(field1);
+                }
+            }
+        }
+    }
+
+    static boolean isDisconnected(Field[][] board) {
+        int[][] visited = new int[board.length][board[0].length];
+        int subGraphNo = UNVISITED;
+        for (int[] ints : visited) {
+            Arrays.fill(ints, UNVISITED);
+        }
+        for (Field[] fields : board) {
+            for (Field field : fields) {
+                if (visited[field.x][field.y] == UNVISITED && !field.isBlocked()) {
+                    if (subGraphNo > UNVISITED) return true;
+                    dfs(field, ++subGraphNo, visited);
+                }
+            }
+        }
+        return false;
+    }
 
     @Test
     void testGetDisconnectingFields() {
@@ -17,7 +51,7 @@ class ObstructionsGeneratorTest {
         int height = 100;
         int obstructionsCount = 0;
         GameBoard gameBoard = new GameBoard(width, height, obstructionsCount);
-        assertFalse(ObstructionsGenerator.isDisconnected(gameBoard.getBoard()));
+        assertFalse(isDisconnected(gameBoard.getBoard()));
         gameBoard.getFieldAt(1, 0).setState(FieldState.BLOCKED);
         gameBoard.getFieldAt(1, 1).setState(FieldState.BLOCKED);
         gameBoard.getFieldAt(0, 1).setState(FieldState.BLOCKED);
@@ -26,7 +60,7 @@ class ObstructionsGeneratorTest {
         gameBoard.getFieldAt(2, 2).setState(FieldState.BLOCKED);
         gameBoard.getFieldAt(2, 1).setState(FieldState.BLOCKED);
         gameBoard.getFieldAt(2, 0).setState(FieldState.BLOCKED);
-        assertTrue(ObstructionsGenerator.isDisconnected(gameBoard.getBoard()));
+        assertTrue(isDisconnected(gameBoard.getBoard()));
     }
 
     @Test
